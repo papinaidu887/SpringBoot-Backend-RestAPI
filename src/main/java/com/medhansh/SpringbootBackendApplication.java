2c5bd6e808f90cbaffba520c477ed87282082c7b
naidu <papinaidu887@gmail.com>
2022-04-03 16:30:59 +0530

    changed the main method

--- src/main/java/com/medhansh/SpringbootBackendApplication.java
+++ src/main/java/com/medhansh/SpringbootBackendApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
+	
+	public static void add2()
+	{
+		System.out.println("static method...................");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
@@ -13,6 +18,8 @@ public class SpringbootBackendApplication {
 		System.out.println(" The entry point of spring boot main method...........");
 		
 		System.out.println("the entry of first methos is main method.......");
+		
+		System.out.println("hiiiiiiiiiiiiiiiiiiiii");
 	}
 
 }
